bb1345fad978db506e9a24ee61b50e894807abcd
BillyBernard <billybernard54@gmail.com>
2021-10-04 12:48:42 -0600

    Display our state within the UI

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,12 @@ export const ADD_ONE = "ADD_ONE";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
 
+// export const initialState = {
+//     total: 0,
+//     operation: "+",
+//     memory: 0
+// }
+
 export const addOne = () => {
     return({type:ADD_ONE});
 }
